d0c9577d4299e11f79332005ecadd02a81284d02
Tristan Emerson <tristan.emerson@echoman.com>
2019-03-17 21:47:03 -0400

    more ssl stuff

--- server/routes/board.routes.js
+++ server/routes/board.routes.js
@@ -11,8 +11,4 @@ router.route('/boards/:boardSlug/users').get(BoardController.getUsersOnBoard);
 router.route('/boards/:boardSlug/users').post(BoardController.addUserToBoard);
 router.route('/boards/:boardSlug/users/:username').post(BoardController.removeUserFromBoard);
 
-router.route('/.well-known/acme-challenge/b05fe7588a112ed2d0d0da28150684c9').get((req, res) => {
-  res.send('b05fe7588a112ed2d0d0da28150684c9.zYZWbDQJDEDNxRwYj5o9Qj9em3QpqDrnZeAUcQPSGfU');
-});
-
 export default router;
