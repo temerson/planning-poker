677b4f27b7dfbdfcf718537a5f6422caa72e4c20
Tristan Emerson <tristan.emerson@echoman.com>
2019-11-11 21:39:48 -0500

    simplify header

--- client/src/modules/App/components/Header.js
+++ client/src/modules/App/components/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { Link } from 'react-router';
-import { getCookies, getUsername } from '../reducers';
 import backgroundImage from '../header-bk.png';
-import { getUserRequest } from '../actions';
 
 const Wrapper = styled.div`
   background: #eee url(${backgroundImage}) center;
@@ -36,55 +32,15 @@ const Title = styled.h1`
   }
 `;
 
-const Welcome = styled.div`
-  font-weight: 500;
-  font-size: 22px;
-  color: #FFF;
-`;
+const Header = () => (
+  <Wrapper>
+    <Title>
+      <Link to="/">Planning Poker</Link>
+      <Link to="/who-dis" className="logout">
+        <i className="material-icons">exit_to_app</i>
+      </Link>
+    </Title>
+  </Wrapper>
+);
 
-class Header extends React.Component {
-  static propTypes = {
-    cookies: PropTypes.object.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    username: PropTypes.string,
-  };
-
-  componentDidMount() {
-    const { cookies, dispatch, username } = this.props;
-    const userId = cookies.userId;
-
-    if (userId && !username) {
-      dispatch(getUserRequest(userId));
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { cookies, dispatch, username } = nextProps;
-    const { userId } = cookies;
-
-    if (userId && !username) {
-      dispatch(getUserRequest(userId));
-    }
-  }
-
-  render() {
-    const { username } = this.props;
-    return (
-      <Wrapper>
-        <Title>
-          <Link to="/">Planning Poker</Link>
-          <Link to="/who-dis" className="logout">
-            <i className="material-icons">exit_to_app</i>
-          </Link>
-        </Title>
-
-        {username && <Welcome>Welcome {username}!</Welcome>}
-      </Wrapper>
-    );
-  }
-}
-
-export default connect((state) => ({
-  username: getUsername(state),
-  cookies: getCookies(state),
-}))(Header);
+export default Header;
