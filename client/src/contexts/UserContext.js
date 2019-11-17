40674889922b7c7214aed9f4d455c10b8ba70e04
Tristan Emerson <tristan.emerson@echoman.com>
2019-11-16 19:23:52 -0500

    use hooks in UserContext

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const UserContext = React.createContext({});
 
-export class UserProvider extends React.Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  }
+export const UserProvider = ({ children }) => {
+  const [username, setUsername] = useState(window.sessionStorage.getItem('username'));
+  useEffect(() => {
+    window.sessionStorage.setItem('username', username);
+    return () => window.sessionStorage.setItem('username', null);
+  }, [username]);
 
-  _getUsername = () => window.sessionStorage.getItem('username');
+  return (
+    <UserContext.Provider
+      value={{
+        getUsername: () => username,
+        setUsername: setUsername,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
 
-  _setUsername = username => window.sessionStorage.setItem('username', username);
-
-  render() {
-    return (
-      <UserContext.Provider
-        value={{
-          getUsername: this._getUsername,
-          setUsername: this._setUsername,
-        }}
-      >
-        {this.props.children}
-      </UserContext.Provider>
-    )
-  }
+UserProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 }
