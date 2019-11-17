5ce1d2b5b8ca26c0562cfc6ae2d5f61db7b1cdce
Tristan Emerson <tristan.emerson@echoman.com>
2019-11-16 19:26:50 -0500

    fix some routing issues

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import { WebsocketProvider } from './contexts/WebsocketContext';
 import Routes from './Routes';
 import './main.css';
 
-const requireUsername = (nextState, replace) => isRegistered => {
+const requireUsername = (nextState, replace) => {
+  // TODO: it would be nice to pull this from UserContext, or make that more DRY
+  const isRegistered = !!window.sessionStorage.getItem('username');
   if (!isRegistered) {
     replace({
       pathname: '/who-dis',
@@ -21,11 +23,7 @@ export default function App(props) {
     <Provider store={props.store}>
       <UserProvider>
         <WebsocketProvider>
-          <Routes
-            history={browserHistory}
-            onChange={(prev, next, replace) => requireUsername(next, replace)}
-            onEnter={requireUsername}
-          />
+          <Routes history={browserHistory} checkUser={requireUsername} />
         </WebsocketProvider>
       </UserProvider>
     </Provider>
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Redirect, Route, Router, IndexRoute } from 'react-router';
-import useUser from './contexts/useUser';
 import App from './modules/App/App';
 import Boards from './modules/Board/pages/Boards';
 import RegisterBoard from './modules/Board/pages/RegisterBoard';
@@ -9,34 +8,29 @@ import Board from './modules/Board/pages/Board';
 import BoardList from './modules/Board/pages/BoardList';
 import WhoDis from './modules/App/WhoDis';
 
-const Routes = ({ history, onEnter, onChange }, ) => {
-  const user = useUser();
-  const isRegistered = !!user.getUsername();
-
-  return (
-    <Router history={history}>
-      <Redirect from="/" to="/boards" />
-      <Route path="/" component={App}>
-        <Route path="who-dis" component={WhoDis} />
-        <Route
-          path="boards"
-          component={Boards}
-          onEnter={onEnter(isRegistered)}
-          onChange={onChange(isRegistered)}
-        >
-          <IndexRoute component={BoardList} />
-          <Route path="new-board" component={RegisterBoard} />
-          <Route path=":boardSlug" component={Board}/>
-        </Route>
+const Routes = ({ history, checkUser }, ) => (
+  <Router history={history}>
+    <Redirect from="/" to="/boards" />
+    <Route path="/" component={App}>
+      <Route path="who-dis" component={WhoDis} />
+      <Route
+        path="boards"
+        component={Boards}
+        onEnter={checkUser}
+        onChange={(prev, next, replace) => checkUser(next, replace)}
+      >
+        <IndexRoute component={BoardList} />
+        <Route path="new-board" component={RegisterBoard} />
+        <Route path=":boardSlug" component={Board}/>
       </Route>
-    </Router>
-  );
-}
+    </Route>
+  </Router>
+);
+
 
 Routes.propTypes = {
   history: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onEnter: PropTypes.func.isRequired,
+  checkUser: PropTypes.func.isRequired,
 };
 
 export default Routes;
